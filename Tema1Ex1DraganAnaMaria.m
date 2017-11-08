3fdf0fc306483aca0ac18b9cc6eaa548cb15ca44
anamariadragan <33002917+anamariadragan@users.noreply.github.com>
2017-11-08 18:47:15 +0200

    Update Tema1Ex1DraganAnaMaria.m

--- Tema1Ex1DraganAnaMaria.m
+++ Tema1Ex1DraganAnaMaria.m
@@ -5,8 +5,6 @@ rez2 = 0.02;
 rez3 = 0.002;         
 
 
-
-
 N1= round(T/rez1);    
 t1=rez1*(0:N1-1);      
                       
@@ -16,15 +14,11 @@ semnal1 =-0.25+ 0.75*square(2*pi*F0*t1, 25);
 subplot(3,1,1),plot(t1,semnal1),grid, ylim([-1.5 2]),xlabel('Timp (sec)<rezolutie rez1>') ,ylabel('Amplitude') 
 
 
-
-
 N2= round(T/rez2);
 t2=rez2*(0:N2-1);
 
 semnal2 =-0.25+ 0.75*square(2*pi*F0*t2, 25);
 subplot(3,1,2),plot(t2,semnal2),grid, ylim([-1.5 2]),xlabel('Timp (sec)<rezolutie rez2>') ,ylabel('Amplitude')
-
-
 
 
 
